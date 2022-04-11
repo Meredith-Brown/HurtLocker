e186d6667f3e4cfcc64e31085f7aa26a5886974a
meredith <meredithbrown302@gmail.com>
2022-04-11 15:57:12 -0400

    reading name without extra characters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,27 +33,43 @@ public class Main {
     }
 
     public void createGroceryItems(List<String> objectCreation) {
+        List<String> objectData = new ArrayList<>();
         for (String s : objectCreation) {
-            List<String> objectData = new ArrayList<>();
-            Pattern patternName = Pattern.compile("name.*?[;|%|*|!|@]", Pattern.CASE_INSENSITIVE);
+            Pattern patternName = Pattern.compile("name.*?[;|%|*|!|@|^]", Pattern.CASE_INSENSITIVE);
             Matcher matcherName = patternName.matcher(s);
+            List<String> intermediateN = new ArrayList<>();
             while (matcherName.find()) {
-                objectData.add(matcherName.group(0));
+                intermediateN.add(matcherName.group(0));
             }
-            Pattern patternPrice = Pattern.compile("price.*?[;|%|*|!|@]", Pattern.CASE_INSENSITIVE);
+            for (String s2 : intermediateN) {
+                Pattern patternName2 = Pattern.compile(":.*?[;|%|*|!|@|^]");
+                Matcher matcherName2 = patternName2.matcher(s2);
+                List<String> intermediateN2 = new ArrayList<>();
+                while (matcherName2.find()) {
+                    intermediateN2.add(matcherName2.group(0));
+                }
+                for (String s3 : intermediateN2) {
+                    Pattern patternName3 = Pattern.compile("[a-zA-Z0-9]+");
+                    Matcher matcherName3 = patternName3.matcher(s3);
+                    while (matcherName3.find()) {
+                        objectData.add(matcherName3.group(0));
+                    }
+                }
+            }
+            Pattern patternPrice = Pattern.compile("price.*?[;|%|*|!|@|^]", Pattern.CASE_INSENSITIVE);
             Matcher matcherPrice = patternPrice.matcher(s);
-            List<String> intermediate = new ArrayList<>();
+            List<String> intermediateP = new ArrayList<>();
             while (matcherPrice.find()) {
-                intermediate.add(matcherPrice.group(0));
+                intermediateP.add(matcherPrice.group(0));
             }
-            for (String s2 : intermediate) {
+            for (String s4 : intermediateP) {
                 Pattern patternPrice2 = Pattern.compile("[0-9]*\\.?[0-9][0-9]");
-                Matcher matcherPrice2 = patternPrice2.matcher(s2);
+                Matcher matcherPrice2 = patternPrice2.matcher(s4);
                 while (matcherPrice2.find()) {
                     objectData.add(matcherPrice2.group(0));
                 }
             }
-            Pattern patternType = Pattern.compile("type.*?[;|%|*|!|@]", Pattern.CASE_INSENSITIVE);
+            Pattern patternType = Pattern.compile("type.*?[;|%|*|!|@|^]", Pattern.CASE_INSENSITIVE);
             Matcher matcherType = patternType.matcher(s);
             while (matcherType.find()) {
                 objectData.add(matcherType.group(0));
@@ -63,10 +79,19 @@ public class Main {
             while (matcherEDate.find()) {
                 objectData.add(matcherEDate.group(0));
             }
+            //if (objectData.get()) // NOT NULL
             //GroceryItem groceryItem = new GroceryItem(objectData.get(0), objectData.get(1),
                     //objectData.get(2), objectData.get(3));
             //groceryList.add(groceryItem); // IF NOT NULL
-            System.out.println(objectData);
+        }
+        for (int i = 0; i < objectData.size(); i++) {
+            if (objectData.get(i) == null) {
+                objectData.remove(i);
+                System.out.println("REMOVE");
+                i--;
+            } else {
+                System.out.println(objectData.get(i));
+            }
         }
     }
 
