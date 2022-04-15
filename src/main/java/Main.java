079b2aa05bf85a9af313a25bc449027d19f81a63
meredith <meredithbrown302@gmail.com>
2022-04-15 09:20:53 -0400

    printing, unformatted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -109,34 +111,95 @@ public class Main { // TODO - write tests!!!
     }
 
     public String printGroceryList(List<GroceryItem> groceryList) {
+        // MAPS: key = price, value = count @ that price
         int milkCount = 0;
+        Map<String, Integer> milkMap = new HashMap<>();
         int breadCount = 0;
+        Map<String, Integer> breadMap = new HashMap<>();
         int cookiesCount = 0; // TODO - account for 0 in Cookie
+        Map<String, Integer> cookiesMap = new HashMap<>();
         int applesCount = 0;
+        Map<String, Integer> applesMap = new HashMap<>();
         for (GroceryItem g : groceryList) {
             Pattern patternMilk = Pattern.compile("milk", Pattern.CASE_INSENSITIVE);
             Matcher matcherMilk = patternMilk.matcher(g.name);
             while (matcherMilk.find()) {
                 milkCount++;
+                if (milkMap.containsKey(g.price)) {
+                    int current = milkMap.get(g.price);
+                    milkMap.replace(g.price, current + 1);
+                } else {
+                    milkMap.put(g.price, 1);
+                }
             }
             Pattern patternBread = Pattern.compile("bread", Pattern.CASE_INSENSITIVE);
             Matcher matcherBread = patternBread.matcher(g.name);
             while (matcherBread.find()) {
                 breadCount++;
+                if (breadMap.containsKey(g.price)) {
+                    int current = breadMap.get(g.price);
+                    breadMap.replace(g.price, current + 1);
+                } else {
+                    breadMap.put(g.price, 1);
+                }
             }
             Pattern patternCookie = Pattern.compile("cookie", Pattern.CASE_INSENSITIVE);
             Matcher matcherCookie = patternCookie.matcher(g.name);
             while (matcherCookie.find()) {
                 cookiesCount++;
+                if (cookiesMap.containsKey(g.price)) {
+                    int current = cookiesMap.get(g.price);
+                    cookiesMap.replace(g.price, current + 1);
+                } else {
+                    cookiesMap.put(g.price, 1);
+                }
             }
             Pattern patternApples = Pattern.compile("apples", Pattern.CASE_INSENSITIVE);
             Matcher matcherApples = patternApples.matcher(g.name);
             while (matcherApples.find()) {
                 applesCount++;
+                if (applesMap.containsKey(g.price)) {
+                    int current = applesMap.get(g.price);
+                    applesMap.replace(g.price, current + 1);
+                } else {
+                    applesMap.put(g.price, 1);
+                }
             }
         }
-        String output = ("Milk         Qty. " + milkCount + "Bread         Qty. " + breadCount +
-                "Cookies         Qty. " + cookiesCount + "Apples         Qty. " + applesCount);
+        // TODO - add actual formatting
+        System.out.println("Milk           Qty. " + milkCount);
+        System.out.println("================             ================");
+        String[] milkKeys = milkMap.keySet().toArray(new String[0]);
+        for (String str : milkKeys) {
+            System.out.println(str + "        Qty. " + milkMap.get(str));
+            System.out.println("---------------");
+        }
+        System.out.println("Bread           Qty. " + breadCount);
+        System.out.println("================             ================");
+        String[] breadKeys = breadMap.keySet().toArray(new String[0]);
+        for (String str : breadKeys) {
+            System.out.println(str + "        Qty. " + breadMap.get(str));
+            System.out.println("---------------");
+        }
+        System.out.println("Cookies           Qty. " + cookiesCount);
+        System.out.println("================             ================");
+        String[] cookiesKeys = cookiesMap.keySet().toArray(new String[0]);
+        for (String str : cookiesKeys) {
+            System.out.println(str + "        Qty. " + cookiesMap.get(str));
+            System.out.println("---------------");
+        }
+        System.out.println("Apples           Qty. " + applesCount);
+        System.out.println("================             ================");
+        String[] applesKeys = applesMap.keySet().toArray(new String[0]);
+        for (String str : applesKeys) {
+            System.out.println(str + "        Qty. " + applesMap.get(str));
+            System.out.println("---------------");
+        }
+
+
+        String output = ("Milk           Qty. " + milkCount + "\n" + "Bread          Qty. " + breadCount
+                + "\n" + "Cookies        Qty. " + cookiesCount + "\n" + "Apples         Qty. " +
+                applesCount + "\n");
         return output;
     }
 
